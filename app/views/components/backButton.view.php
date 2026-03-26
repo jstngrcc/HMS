7db4c44cfda5d5b92e5a5a45cd1d621c4b711e72
jeiya1 <aniagjoseph593@gmail.com>
2026-03-27 03:30:03 +0800

    feat: updated backButton

--- app/views/components/backButton.view.php
+++ app/views/components/backButton.view.php
@@ -1,4 +1,4 @@
-<button class="rounded-sm hover:bg-[#3F321F] shadow-2xl py-2 px-4 bg-[#C39C4D] transition-colors w-22" onclick="window.history.back()">
+<button class="rounded-sm hover:bg-[#3F321F] self-stretch shadow-2xl py-2 px-4 bg-[#C39C4D] transition-colors w-22" onclick="window.history.back()">
     <p
         class="transition-all duration-300 text-white hover:text-white hover:[text-shadow:0_0_8px_rgba(255,255,255,0.9)]">
         BACK
